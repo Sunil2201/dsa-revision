5a90d3378cb6c9e3e1b585977565f75c76e5d006
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-17 15:19:13 +0530

    Input/Output

---
+++ src/basics/Input.java
@@ -0,0 +1,13 @@
+package basics;
+
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your roll no: ");
+        int roll_no = input.nextInt();
+        System.out.println(roll_no);
+    }
+}
