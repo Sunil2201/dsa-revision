a81c1581e56f854bba4ea5f0afd5121ae39ed912
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-21 19:00:34 +0530

    Factorial of a number

---
+++ src/functions/Factorial.java
@@ -0,0 +1,19 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        System.out.println("Factorial of " +n+ " is: " +factorial(n));
+    }
+    static int factorial(int n){
+        int factorial = 1;
+        for(int i=n; i>0; i--){
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
