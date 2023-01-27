de7a66cae50568d6c693ceaab272e80aebdd24bb
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-27 19:48:45 +0530

    refactor: duplicates is RBS

--- src/binary_search/RBS.java
+++ src/binary_search/RBS.java
@@ -36,6 +36,41 @@ public class RBS {
         }
         return -1;
     }
+    static int findPivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(mid > start && arr[mid] < arr[mid-1]){
+                return mid-1;
+            }
+            // elements at middle, start, end are equal just skip the duplicates
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                // skip the duplicates
+                // what if these elements were the pivot??
+                // check if start is pivot
+                if(arr[start] > arr[start+1]){
+                    return start;
+                }
+                start++;
+                //check if end is pivot
+                if(arr[end] < arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+
+        }
+        return -1;
+    }
     static int binarySearch(int[] arr, int target, int start, int end){
         while(start<=end){
             int mid = start + (end-start)/2;
