c271772109159cc80ef0529f3a958784341a041b
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-30 18:39:40 +0530

    Selection Sort

---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {3,-1,5,4,-2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            // find the max element in the remaining array and swap with correct index
+            int last = arr.length-1-i;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+     static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for(int i=0; i<=end; i++){
+            if(arr[i] > arr[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
