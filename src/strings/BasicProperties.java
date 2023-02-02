b7b0831eb3846f9ce87d41825f6a1494133225b5
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-02 22:54:32 +0530

    String basics

---
+++ src/strings/BasicProperties.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class BasicProperties {
+    public static void main(String[] args) {
+        String a = "Sunil";
+        String b = "Sunil";
+        a = "Sunil Ballani";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ src/strings/Comparator.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class Comparator {
+    public static void main(String[] args) {
+        String a = "Sunil";
+        String b = "Sunil";
+        System.out.println(a==b);
+    }
+}
