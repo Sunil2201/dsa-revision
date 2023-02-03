7f53a77f37db8c5b6b26c8c2bf7d4f81088e4610
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-03 22:36:32 +0530

    Pretty Printing in Java

---
+++ src/strings/PrettyPrinting.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 445.90987f;
+        System.out.printf("Formatted number is %.2f", a);
+        System.out.println();
+        System.out.printf("Hello my name is %s and I am %s", "Sunil", "hot");
+    }
+}
