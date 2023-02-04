f09392f0ad2f8e3a1b5631d14289ebd4cdd5a25b
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-04 17:22:12 +0530

    Stringbuilder and string methods

---
+++ src/strings/Methods.java
@@ -0,0 +1,10 @@
+package strings;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Sunil Ballani";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
