40ca0a7953e7229e03b2ff00166c65ee46842d69
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-10 22:58:01 +0530

    Fibo Formula

--- src/recursion/Fibo.java
+++ src/recursion/Fibo.java
@@ -2,7 +2,14 @@ package recursion;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibonacci(50));
+//        for(int i=0; i<=11; i++){
+//            System.out.println(fiboFormula(i));
+//        }
+        System.out.println(fiboFormula(50));
+    }
+
+    static int fiboFormula(int n){
+        return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
 
     static long fibonacci(int n){
