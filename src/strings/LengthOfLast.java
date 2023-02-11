900326c765314b93461f27decd1630264dfe817f
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-12 00:11:38 +0530

    Minor changes in some files

--- src/strings/LengthOfLast.java
+++ src/strings/LengthOfLast.java
@@ -8,6 +8,9 @@ public class LengthOfLast {
     }
     static int lengthOfLastWord(String s) {
         String[] arrOfStr = s.split(" ");
+        String name = "Sunil";
+        String newName = name.substring(0, name.length()-1);
+        System.out.println(newName);
         return arrOfStr[arrOfStr.length-1].length();
     }
 }
--- src/strings/Operators.java
+++ src/strings/Operators.java
@@ -8,7 +8,7 @@ public class Operators {
     public static void main(String[] args) {
         System.out.println('a' + 'b');
         System.out.println("a" + "b");
-        System.out.println((char)('a' + 3));
+        System.out.println(('a' + 3));
 
         System.out.println("a" + 1);
 //        this is same as after a few steps "a" + "1"
