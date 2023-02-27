2008109273abbbb1d64b0d3b0eebdd9bcfbe1209
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-27 23:36:34 +0530

    Reverse of number

---
+++ src/recursion/Reverse.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Reverse {
+    static int sum = 0;
+    public static void main(String[] args) {
+        rev(1234);
+        System.out.println(sum);
+    }
+    static void rev(int n){
+        if(n==0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10 + rem;
+        rev(n/10);
+    }
+}
