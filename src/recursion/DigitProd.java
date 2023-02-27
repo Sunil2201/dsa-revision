62bc15afc65356d5546707b96ce880c038f8be3c
Sunil Ballani <ballanisunil123@gmail.com>
2023-02-27 12:01:00 +0530

    Sum and product of digits

---
+++ src/recursion/DigitProd.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class DigitProd {
+    public static void main(String[] args) {
+        System.out.println(prod(1342));
+    }
+
+    static int prod(int n){
+        if(n%10 == n){
+            return n;
+        }
+        return  n%10 * prod(n/10);
+    }
+}
